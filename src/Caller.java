fb8df3d7c26f7f9bac5a698c3cf331674686c49b
jesusinyourtown1 <jesusinyourtown@gmail.com>
2015-11-11 23:37:22 +0200

    minor fixes

--- src/Caller.java
+++ src/Caller.java
@@ -2,6 +2,8 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.Socket;
 
+import static Command.CommandType.NICK;
+
 /**
  * Created by user on 10.11.2015.
  */
@@ -11,7 +13,7 @@ public class Caller {
             Socket socket = new Socket(ipAddr, PORT);
             Connection conn = new Connection(socket);
             conn.sendNickHello(nick);
-            if (conn.receive().getType()== Command.CommandType.NICKHELLO){return conn;} // тип команды NICKHELLO пока не прописан
+            if (conn.receive().getType()== NICK){return conn;}
 
         }
         catch (IOException ioe){ioe.printStackTrace();}
