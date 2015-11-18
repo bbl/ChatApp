7f769a3a29d0ef8d6b70653f72d6e3439fb4391e
Bogdan <metallica127@mail.ru>
2015-11-18 23:01:06 +0200

    Applying observer and threads

--- src/CallListener.java
+++ src/CallListener.java
@@ -14,11 +14,11 @@ public class CallListener{          //начат
    public Connection getConnection(Socket socket) throws IOException {
        Connection c = new Connection(socket);
        if (Protocol.isBusy == true) {                              //если при этом у нас занято
-           c.sendNickBusy(Protocol.nick);
+           c.sendNickBusy(Protocol.NICK);
            c.disconnect();
            c=null;
        } else {
-           c.sendNickHello(Protocol.nick);
+           c.sendNickHello(Protocol.NICK);
            Command tmpCommand = c.receive();
            if (tmpCommand.type == Command.CommandType.NICK) {
                //вывести приветствие клиента на экран
