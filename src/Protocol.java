af19b27e2a02b968b61b6b2d99f26e35d4200e4f
Bogdan <metallica127@mail.ru>
2015-12-09 22:08:56 +0200

    Adding friend from server

--- src/Protocol.java
+++ src/Protocol.java
@@ -7,5 +7,5 @@ public class Protocol {
 	public static boolean isBusy;
 	public static String NICK;
 	public static String curNick; // ник человека, с которым сейчас ведется беседа. убрать из констант, когда появятся множественные диалоги
-	public static ServerConnection c = new ServerConnection();
+	public static ServerConnection serverCon = new ServerConnection();
 }
